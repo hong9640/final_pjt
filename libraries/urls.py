e21584b3b231f13cb859194bd45444dbfdfb85c8
seungju <f.potato.potage@gmail.com>
2025-05-23 13:56:25 +0900

    0523_4

--- libraries/urls.py
+++ libraries/urls.py
@@ -1,12 +1,13 @@
-# libraries/urls.py
-
 from django.urls import path
-from . import views  # libraries 앱의 views.py를 임포트합니다.
+from . import views
 
-app_name = 'libraries'  # URL 네임스페이스를 설정합니다.
+app_name = 'libraries'
 
 urlpatterns = [
-    # 예를 들어, /library/add/1/ 와 같은 URL로 책 ID(book_id)를 받습니다.
     path('add/<int:book_id>/', views.add_to_library, name='add_to_library'),
-    path('remove/<int:book_id>/', views.remove_from_library, name='remove_from_library'),
+    path('remove/to-detail/<int:book_id>/', views.remove_from_library_to_detail, name='remove_from_library_to_detail'),
+    path('remove/to-library/<int:book_id>/', views.remove_from_library_to_mylibrary, name='remove_from_library_to_mylibrary'),
+    path('my-library/', views.my_library, name='my_library'),
+    path('add/<int:book_id>/', views.add_to_library, name='add_to_library'),
+    path('add/ajax/<int:book_id>/', views.add_to_library_ajax, name='add_to_library_ajax'),
 ]
